0d059e18e28535c3c93ede26b83e0f4b95c43e37
Josh <joshmarq2004@gmail.com>
2025-02-11 22:29:18 -0500

    I put the test camera recognition into faceID.py

---
+++ faceID.py
@@ -0,0 +1,26 @@
+import cv2
+import face_recognition
+import random
+
+video_capture = cv2.VideoCapture(0)
+print("Press 'q' to quit.")
+
+while True:
+    ret, frame = video_capture.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    rgb_frame = frame[:, :, ::-1]
+    face_locations = face_recognition.face_locations(rgb_frame)
+
+    for (top, right, bottom, left) in face_locations:
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        
+    cv2.imshow('Current Camera', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
