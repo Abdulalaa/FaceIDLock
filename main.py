af9f5001e7d25919ee0a23658d0656a9aff1e6da
Abdullah <abdallah@mac.mynetworksettings.com>
2025-02-11 20:42:31 -0500

    Authorshop

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+A facial recognition security system that:
+1. Captures and processes facial data
+2. Matches against authorized user database
+3. Controls access based on verification
+
+Authors:
+- Abdullah A.
+- Dev R. 
+- Josh M.
+- Bryan R.
+
+---------------------------------------------                                                
+Secure Facial Recognition Access Control System
+---------------------------------------------
+"""
