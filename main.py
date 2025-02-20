ca42a04007d5e932ef619438b26da5581f7f8b25
Abdullah <abdallah@mac.mynetworksettings.com>
2025-02-20 15:24:32 -0500

    small changes

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 """
-A facial recognition security system that:
-1. Captures and processes facial data
-2. Matches against authorized user database
-3. Controls access based on verification
-
 Authors:
 - Abdullah A.
 - Dev R. 
